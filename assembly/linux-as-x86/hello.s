b2199dc0dc2cff8728f404717ed5ebbc08fa9644
Daekyu Lee <dagui0@gmail.com>
2025-11-18 00:08:27 +0900

    directory structure

---
+++ assembly/linux-as-x86/hello.s
@@ -0,0 +1,19 @@
+.data
+msg: .ascii "Hello, World!\n"
+len = . - msg
+
+.text
+.global _start
+
+_start:
+    # write
+    movl $4, %eax        # System call number for sys_write
+    movl $1, %ebx        # File descriptor 1 (stdout)
+    movl $msg, %ecx      # Address of the string to write
+    movl $len, %edx      # Length of the string (including newline)
+    int $0x80            # Invoke the kernel
+
+    # exit
+    movl $1, %eax       # System call number for sys_exit
+    movl $0, %ebx        # Exit code 0 (success)
+    int $0x80            # invoke the kernel
---
+++ assembly/linux-as-x86_64/hello.s
@@ -0,0 +1,19 @@
+.data
+msg: .asciz "Hello, World!\n"
+.equ len, . - msg
+
+.text
+.global _start
+
+_start:
+    # sys_write (syscall number 1)
+    mov $1, %rax        # System call number for sys_write
+    mov $1, %rdi        # File descriptor 1 (stdout)
+    lea msg(%rip), %rsi # Address of the string to write
+    mov $len, %rdx      # Length of the string (including newline)
+    syscall             # Invoke the kernel
+
+    # sys_exit (syscall number 60)
+    mov $60, %rax       # System call number for sys_exit
+    xor %rdi, %rdi      # Exit code 0 (success)
+    syscall             # invoke the kernel
